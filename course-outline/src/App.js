2caa426995991dbdc6eba74bdc3fef7d65fe7276
Tong Xu <jennytong.xu@gamil.com>
2020-12-14 15:18:37 -0700

    Add static breadcromb navigation and save button

--- course-outline/src/App.js
+++ course-outline/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
+import 'bulma/css/bulma.css';
 import CourseInfo from './components/CourseInfo';
 import LearnOutcome from './components/LearnOutcome';
 import Grade from './components/Grade';
 
 function App() {
+
   return (
-    <div className="App">
-      <CourseInfo />
-      <LearnOutcome />
-      <Grade />
-    </div>
+    <nav className="breadcrumb has-succeeds-separator" aria-label="breadcrumbs">
+      <div>
+        <ul>
+          <li className="is-active"><a href="#" aria-current="page">New Outline</a></li>
+          <li><a href="#">Retrieve Last Saved Outline</a></li>
+        </ul>
+      </div>
+      <div>
+        <button className="button">Save</button>
+      </div>
+      <br></br>
+      <div className="App">
+        <CourseInfo />
+        <LearnOutcome />
+        <Grade />
+      </div>
+    </nav>
   );
 }
 
