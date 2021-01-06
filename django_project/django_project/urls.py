2cf02775a7a39a8fa6e3d3ed0a759b21f25e18cb
Tong Xu <jennytong.xu@gamil.com>
2021-01-05 17:50:38 -0700

    Integrate react and django

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^', include('echos.urls')),
+    path('', include('echos.urls')),
+    path('', TemplateView.as_view(template_name='index.html'))
+
 ]
