e8f9b4df580a7e365783ff54a0c3b4060ebdd570
Tong Xu <jennytong.xu@gamil.com>
2021-01-12 14:10:57 -0700

    removed react template

--- course-outline/django_project/urls.py
+++ course-outline/django_project/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('echos.urls')),
@@ -7,5 +6,4 @@ urlpatterns = [
     path('', include('coursegrades.urls')),
     path('', include('learningoutcomes.urls')),
     path('', include('graduateattributes.urls')),
-    path('', TemplateView.as_view(template_name='index.html'))
 ]
