b5d80410ec4a875559ec9379b445077f63bbe743
Mike Lasby <mklasby@ucalgary.ca>
2021-01-12 09:36:48 -0700

    integrating react w/ django

--- course-outline/react/src/components/LearnOutcome.js
+++ course-outline/react/src/components/LearnOutcome.js
@@ -2,29 +2,62 @@ import React, { useEffect } from 'react';
 import MaterialTable from 'material-table';
 import { useState, useReducer } from "react";
 import { TableIcons } from "./TableConstants";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import axios from 'axios';
 
-function setLearningOutcomes(res) {
-  console.log(res)
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
 }
 
-const OutcomesTable = () => {
 
+const OutcomesTable = () => {
   const [data, setData] = useState([])
   const [hasError, setErrors] = useState(false)
 
+  const id = 3;
+
   useEffect(() => {
     async function fetchOutcomes() {
-      const res = await fetch("http://127.0.0.1:8000/api/learningoutcomes/")
-      console.log(res.json());
-      res.json().then(res => setLearningOutcomes(res.response)).catch(err => setErrors(err));
+      console.log(id);
+      axios.defaults.xsrfCookieName = 'csrftoken';
+      axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+      // use cookies for targetting specific detail api view
+      // axios.get("http://127.0.0.1:8000/api/learningoutcomes/", { headers: { "X-CSRFToken": getCookie('csrftoken') } })
+      axios.get("http://127.0.0.1:8000/api/learningoutcomes/")
+        .then(function (response) {
+          console.log(response.data);
+          setData(response.data.filter(x => x.courseNum == 2))
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+
+      // OTHER VALID OPTIONS: 
+      // const res = await fetch("http://127.0.0.1:8000/api/learningoutcomes/" + id, { headers: { "Content-Type": 'application/json' } })
+      // const res = await fetch("http://127.0.0.1:8000/api/learningoutcomes/");
+      // res.json().then(res => setData(res)).catch(err => setErrors(err));
     }
     fetchOutcomes();
   }, [])
 
-  const paddingZero = {
-    padding: "0px"
-  }
-
+  console.log(data);
   return (
     <MaterialTable
       style={{ padding: '0px' }}
