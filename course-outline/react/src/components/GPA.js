5691521f1dca1ae252cc670c416523c172b67426
Tong Xu <jennytong.xu@gamil.com>
2021-01-13 20:14:48 -0700

    re-add total for grade components

--- course-outline/react/src/components/GPA.js
+++ course-outline/react/src/components/GPA.js
@@ -11,48 +11,11 @@ import {
 import axios from 'axios';
 
 
-function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === (name + '=')) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
-    }
-  }
-  return cookieValue;
-}
-
-
 const GPATable = () => {
   const [data, setData] = useState([])
   const [hasError, setErrors] = useState(false)
 
   const id = 2;
-
-  useEffect(() => {
-    async function fetchGPA() {
-      console.log(id);
-      axios.defaults.xsrfCookieName = 'csrftoken';
-      axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-      
-      axios.get("http://127.0.0.1:8000/api/gpaconversion/")
-        .then(function (response) {
-          console.log(response.data);
-          setData(response.data)
-        })
-        .catch(function (error) {
-          console.log(error)
-        })
-
-     
-    }
-    fetchGPA();
-  }, [])
 
   console.log(data);
   return (
