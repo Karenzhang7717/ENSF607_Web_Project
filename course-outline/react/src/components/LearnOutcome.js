82ad39de065e63bb0848b891bcd59514f7d621f8
Mike Lasby <mklasby@ucalgary.ca>
2021-01-13 22:18:34 -0700

    learning outcome getting

--- course-outline/react/src/components/LearnOutcome.js
+++ course-outline/react/src/components/LearnOutcome.js
@@ -10,47 +10,31 @@ import {
 } from "react-router-dom";
 import axios from 'axios';
 import { LEARNINGOUTCOME_URL } from "../constants/index";
-// import { getCourseNum } from "./Courseinfo";
-
-
-// function getCookie(name) {
-
-//   let cookieValue = null;
-//   if (document.cookie && document.cookie !== '') {
-//     const cookies = document.cookie.split(';');
-//     for (let i = 0; i < cookies.length; i++) {
-//       const cookie = cookies[i].trim();
-//       // Does this cookie string begin with the name we want?
-//       if (cookie.substring(0, name.length + 1) === (name + '=')) {
-//         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-//         break;
-//       }
-//     }
-//   }
-//   return cookieValue;
-// }
 
 const OutcomesTable = (props) => {
   const courseNum = props.courseNum;
   const newOutline = props.newOutline;
   console.log("courseNum " + courseNum);
   const [data, setData] = useState([]);
+  const [existingData, setExistingData] = useState([]);
   const [hasError, setErrors] = useState(false);
 
   useEffect(() => {
     async function fetchOutcomes() {
       axios.get(LEARNINGOUTCOME_URL)
         .then(function (response) {
-          setData(response.data.filter(x => x.courseNum == courseNum));
+          console.log(response.data);
+          setExistingData(response.data.filter(x => x.courseNum == courseNum));
         })
         .catch(function (error) {
           console.log(error)
         })
     }
     if (!newOutline) {
+      console.log("fetching data")
       fetchOutcomes();
     }
-    if (data) {
+    if (data && newOutline) {
       console.log("data is being sent");
       props.onChange(data);
     }
@@ -95,6 +79,7 @@ const OutcomesTable = (props) => {
   // console.log(data);
   // console.log("course number from courseinfos: " + getCourseNum())
 
+  console.log(existingData);
 
   return (
     <MaterialTable
@@ -110,8 +95,8 @@ const OutcomesTable = (props) => {
         ]}
       title="Learning Outcomes"
       icons={TableIcons}
-      data={data}
-      editable={{
+      data={newOutline ? data : existingData}
+      editable={newOutline ? {
         onRowAdd: newData =>
           new Promise((resolve, reject) => {
             setTimeout(() => {
@@ -130,7 +115,7 @@ const OutcomesTable = (props) => {
               handleRowDelete(oldData);
             }, 1000)
           }),
-      }}
+      } : {}}
     />
   )
 }
--- course-outline/react/src/components/SavedForm.js
+++ course-outline/react/src/components/SavedForm.js
@@ -1,5 +1,5 @@
 import CourseInfoStatic from './CourseInfoStatic';
-import LearnOutcomeStatic from './LearnOutcomeStatic';
+import LearnOutcome from './LearnOutcome';
 import GradeStatic from './GradeStatic';
 import GPAStatic from './GPAStatic';
 import Notes from './Notes';
@@ -9,17 +9,18 @@ import { useParams } from 'react-router-dom';
 
 function SavedForm() {
 
-  const courseNum = useParams();
+  const { courseNum } = useParams();
   console.log(courseNum);
 
   return (
 
     <div className="saved-form">
       <h1>1. Course Information</h1>
-      {CourseInfoStatic(courseNum)}
+      <CourseInfoStatic courseNum={courseNum} />
+      {/* {CourseInfoStatic(courseNum)} */}
       <h1>2. Learning Outcomes</h1>
-      <p>At the end of this course, you will be able to:</p>        
-      <LearnOutcomeStatic />
+      <p>At the end of this course, you will be able to:</p>
+      <LearnOutcome courseNum={courseNum} newOutline={false} />
       <br></br>
       <p style={{ wordBreak: 'break-all', whiteSpace: "normal" }}>Graduate Attributes are generic characteristics specified by the CEAB (Canadian Engineering Accreditation Board), expected to be exhibited by graduates of Canadian engineering schools. This table summarizes how the Learning Outcomes relate to key Graduate Attributes addressed in this course.</p>
       <br></br>
