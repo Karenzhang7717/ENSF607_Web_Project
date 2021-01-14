dd0c0b0a43aecfb92859b0c16186316b632d896c
Tong Xu <jennytong.xu@gamil.com>
2021-01-13 20:40:25 -0700

    Edit style

--- course-outline/react/src/components/SavedForm.js
+++ course-outline/react/src/components/SavedForm.js
@@ -18,7 +18,8 @@ function SavedForm() {
       <h1>1. Course Information</h1>
       {CourseInfoStatic(courseNum)}
       <h1>2. Learning Outcomes</h1>
-      <p>At the end of this course, you will be able to:</p>        <LearnOutcomeStatic />
+      <p>At the end of this course, you will be able to:</p>        
+      <LearnOutcomeStatic />
       <br></br>
       <p style={{ wordBreak: 'break-all', whiteSpace: "normal" }}>Graduate Attributes are generic characteristics specified by the CEAB (Canadian Engineering Accreditation Board), expected to be exhibited by graduates of Canadian engineering schools. This table summarizes how the Learning Outcomes relate to key Graduate Attributes addressed in this course.</p>
       <br></br>
@@ -30,6 +31,7 @@ function SavedForm() {
       <GradeStatic />
       <Notes />
       <GPAStatic />
+      <br></br>
     </div>
 
   );
