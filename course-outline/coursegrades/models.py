23b0a5d9d6a1cd99571c61a737371b0677d96ecb
Karenzhang7717 <64809520+Karenzhang7717@users.noreply.github.com>
2021-01-13 20:55:22 -0700

    added courseNum to model

--- course-outline/coursegrades/models.py
+++ course-outline/coursegrades/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class CourseGrade(models.Model):
+    courseNum = models.CharField(max_length=10, unique=True)
     courseComponent = models.CharField(max_length=50)
     courseOutcomes = models.CharField(max_length=50)
     courseWeight = models.CharField(max_length=10)
